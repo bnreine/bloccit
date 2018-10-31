16ff741c5d1cb93eafd57072f40227971e0fbbdd
Benjamin <bnreinecke0209@gmail.com>
2018-10-31 12:59:54 -0400

    Assignment: Introduction to Express: Completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,23 +1,29 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = base + "marco";
 
 describe("routes : static", () => {
 
-//#1
   describe("GET /", () => {
-
-//#2
     it("should return status code 200", (done) => {
-
-//#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-
-//#4
         done();
       });
     });
-
   });
+
+
+      describe("GET /marco", () => {
+        it("should return status code 200 and polo for body", (done) => {
+          request.get(marco, (err, res, body) => {
+            expect(res.statusCode).toBe(200);
+            expect(body).toBe("polo");
+            done();
+          });
+        });
+      });
+
+
 });
