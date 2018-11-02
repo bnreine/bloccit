7a208cd8e875b5211f93514c6961e17adb5bb425
Benjamin <bnreinecke0209@gmail.com>
2018-11-02 16:57:51 -0400

    Assignment: The Topic Resource Part: 2: 1/2 done

---
+++ spec/integration/advertisement_spec.js
@@ -0,0 +1,66 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/advertisements/";
+const sequelize = require("../../src/db/models/index").sequelize;
+const Advertisement = require("../../src/db/models").Advertisement;
+
+
+
+describe("routes : advertisements", () => {
+
+  beforeEach((done) => {
+    this.advertisement;
+    sequelize.sync({force: true}).then((res) => {
+
+     Advertisement.create({
+       title: "JS Frameworks",
+       description: "There is a lot of them"
+     })
+      .then((advertisement) => {
+        this.advertisement = advertisement;
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
+
+    });
+
+  });
+
+
+
+  describe("GET /advertisements", () => {
+
+    it("should return a status code 200 and all advertisements", (done) => {
+           request.get(base, (err, res, body) => {
+             expect(res.statusCode).toBe(200);
+             expect(err).toBeNull();
+             expect(body).toContain("Advertisements");
+             expect(body).toContain("JS Frameworks");
+             done();
+           });
+         });
+       });
+
+
+  describe("GET /advertisements/new", () => {
+
+         it("should render a new advertisement form", (done) => {
+           request.get(`${base}new`, (err, res, body) => {
+             expect(err).toBeNull();
+             expect(body).toContain("New Advertisement");
+             done();
+           });
+         });
+
+  });
+
+
+
+
+
+
+
+});
