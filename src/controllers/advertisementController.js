f1eb6563712f9a3fc790117bf6cc204deb121512
Benjamin <bnreinecke0209@gmail.com>
2018-11-04 17:11:06 -0500

    Assignment: The Topic Resource Part: 2: 7/8 done

--- src/controllers/advertisementController.js
+++ src/controllers/advertisementController.js
@@ -15,4 +15,30 @@ module.exports = {
     res.render("advertisements/new");
   },
 
+  create(req, res, next){
+    let newAdvertisement = {
+      title: req.body.title,
+      description: req.body.description
+    };
+    advertisementQueries.addAdvertisement(newAdvertisement, (err, advertisement) => {
+      if(err){
+        res.redirect(500, "/advertisements/new");
+      } else {
+        res.redirect(303, `/advertisements/${advertisement.id}`);
+      }
+    });
+  },
+
+  show(req, res, next){
+    advertisementQueries.getAdvertisement(req.params.id, (err, advertisement) => {
+      if(err || advertisement == null){
+        res.redirect(404, "/");
+      } else {
+        res.render("advertisements/show", {advertisement});
+      }
+    });
+  }
+
+
+
 }
--- src/db/queries.advertisements.js
+++ src/db/queries.advertisements.js
@@ -1,17 +1,35 @@
 const Advertisement = require("./models").Advertisement;
 
 module.exports = {
-
-//#1
   getAllAdvertisements(callback){
     return Advertisement.all()
-
-//#2
     .then((advertisements) => {
       callback(null, advertisements);
     })
     .catch((err) => {
       callback(err);
     })
+  },
+  addAdvertisement(newAdvertisement, callback){
+    return Advertisement.create({
+      title: newAdvertisement.title,
+      description: newAdvertisement.description
+    })
+    .then((advertisement) => {
+      callback(null, advertisement);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  },
+  getAdvertisement(id, callback){
+    return Advertisement.findById(id).then((advertisement) => {
+      callback(null, advertisement);
+    })
+    .catch((err) => {
+      callback(err);
+    })
   }
+
+
 }
