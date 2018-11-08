fffe23d7ce85a00e6b2967a65fe5d922509933e4
Benjamin <bnreinecke0209@gmail.com>
2018-11-08 14:50:48 -0500

    Checkpoint: Authorization: Completed

---
+++ spec/unit/topic_spec.js
@@ -0,0 +1,94 @@
+const sequelize = require("../../src/db/models/index").sequelize;
+const Topic = require("../../src/db/models").Topic;
+const Post = require("../../src/db/models").Post;
+const User = require("../../src/db/models").User;
+
+describe("Topic", () => {
+
+  beforeEach((done) => {
+       this.topic;
+       this.post;
+       this.user;
+
+       sequelize.sync({force: true}).then((res) => {
+
+  // #2
+         User.create({
+           email: "starman@tesla.com",
+           password: "Trekkie4lyfe"
+         })
+         .then((user) => {
+           this.user = user; //store the user
+
+  // #3
+           Topic.create({
+             title: "Expeditions to Alpha Centauri",
+             description: "A compilation of reports from recent visits to the star system.",
+
+  // #4
+             posts: [{
+               title: "My first visit to Proxima Centauri b",
+               body: "I saw some rocks.",
+               userId: this.user.id
+             }]
+           }, {
+
+  // #5
+             include: {
+               model: Post,
+               as: "posts"
+             }
+           })
+           .then((topic) => {
+             this.topic = topic; //store the topic
+             this.post = topic.posts[0]; //store the post
+             done();
+           })
+         })
+       });
+     });
+
+
+
+  describe("#create()", () => {
+
+      it("should create and store a topic object with a title and description", (done) => {
+        Topic.create({
+          title: "Gemini",
+          description: "A month"
+        })
+        .then((topic) => {
+          expect(topic.title).toBe("Gemini");
+          expect(topic.description).toBe("A month");
+          done();
+        })
+        .catch((err) => {
+          console.log(err);
+          done();
+        });
+      });
+
+    });
+
+
+
+    describe("#getPosts()", () => {
+
+      it("should return the associated posts", (done) => {
+
+        this.topic.getPosts()
+        .then((associatedPosts) => {
+          expect(associatedPosts[0].body).toBe("I saw some rocks.");
+          done();
+        });
+
+      });
+
+    });
+
+
+
+
+
+
+});
