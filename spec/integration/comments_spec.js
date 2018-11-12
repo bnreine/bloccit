842c5e28a52fbb1e030bb49898b25e150cad7453
Benjamin <bnreinecke0209@gmail.com>
2018-11-12 12:17:54 -0500

    Assignment: Comments: Completed

--- spec/integration/comments_spec.js
+++ spec/integration/comments_spec.js
@@ -200,7 +200,7 @@ describe("member performing CRUD actions for Comment", () => {
 // #3
      describe("POST /topics/:topicId/posts/:postId/comments/:id/destroy", () => {
 
-       it("should delete the comment with the associated ID", (done) => {
+       it("should delete the comment if it belongs to the user", (done) => {
          Comment.all()
          .then((comments) => {
            const commentCountBeforeDelete = comments.length;
@@ -223,7 +223,55 @@ describe("member performing CRUD actions for Comment", () => {
 
        });
 
+
+       it("should not delete a comment from another user", (done) => {
+
+         request.get({           // mock authentication
+           url: "http://localhost:3000/auth/fake",
+           form: {
+             id: 0 //logs out first
+           }
+         },
+           (err, res, body) => {
+
+           }
+         );
+         request.get({           // mock authentication
+           url: "http://localhost:3000/auth/fake",
+           form: {
+             role: "member",     //authenticate member that doesn't own comment
+             id: 2
+           }
+         },
+           (err, res, body) => {
+             //done();
+           }
+         );
+         Comment.all()
+         .then((comments) => {
+           const commentCountBeforeDelete = comments.length;
+
+           expect(commentCountBeforeDelete).toBe(1);
+
+           request.post(
+            `${base}${this.topic.id}/posts/${this.post.id}/comments/${this.comment.id}/destroy`,
+             (err, res, body) => {
+             //expect(res.statusCode).toBe(302);
+             Comment.all()
+             .then((comments) => {
+               expect(err).toBeNull();
+               expect(comments.length).toBe(commentCountBeforeDelete);
+               done();
+             })
+
+           });
+         })
+
+       })
+
      });
+
+
 
    });
 
@@ -238,8 +286,62 @@ describe("member performing CRUD actions for Comment", () => {
 
 
 
+//**************start admin context***************************************************
 
 
+describe("admin performing CRUD actions for Comment", () => {
+
+     beforeEach((done) => {    // before each suite in this context
+       request.get({           // mock authentication
+         url: "http://localhost:3000/auth/fake",
+         form: {
+           role: "admin",     // authenticate admin that doesn't own comment
+           id: 3
+         }
+       },
+         (err, res, body) => {
+           done();
+         }
+       );
+     });
+
+
+     describe("POST /topics/:topicId/posts/:postId/comments/:id/destroy", () => {
+
+       it("should delete other users' comments if admin", (done) => {
+         Comment.all()
+         .then((comments) => {
+           const commentCountBeforeDelete = comments.length;
+
+           expect(commentCountBeforeDelete).toBe(1);
+
+           request.post(
+            `${base}${this.topic.id}/posts/${this.post.id}/comments/${this.comment.id}/destroy`,
+             (err, res, body) => {
+             expect(res.statusCode).toBe(302);
+             Comment.all()
+             .then((comments) => {
+               expect(err).toBeNull();
+               expect(comments.length).toBe(commentCountBeforeDelete - 1);
+               done();
+             })
+
+           });
+         })
+
+       });
+
+    });
+
+
+
+
+  });
+
+
+
+
+//**************end admin context***************************************************
 
 
 
